4c9f3f7639815fe279fe7e00c306f0acf79f6ce8
lembolov9 <tomas080808>
2019-10-22 02:30:06 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {render} from 'react-dom';
+import App from "./components/App.js"
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import {INITIAL_STATE} from "./constants/initial-state";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index"
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const newCityId = localStorage['weather-app'] ? JSON.parse(localStorage['weather-app'])['newCityId']: 1;
+const cities = localStorage['weather-app'] ? JSON.parse(localStorage['weather-app'])['cityList']: [];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const state = {
+    ...INITIAL_STATE,
+    cityList: {
+        ...INITIAL_STATE.cityList,
+        cities ,
+        newCityId
+    }
+};
+
+const store  = createStore(rootReducer, state, applyMiddleware(thunk));
+
+render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+    document.getElementById("root")
+);
+
+store.subscribe(() => {
+    localStorage.setItem('weather-app',JSON.stringify({
+            'cityList': store.getState().cityList.cities.map(city => ({id: city.id, name: city.name})),
+            'newCityId': store.getState().cityList.newCityId
+            }
+        )
+    );
+});
+
 serviceWorker.unregister();
